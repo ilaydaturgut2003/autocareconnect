4d5af85ecef83fb6257c0b32504c808527aee383
KyleMullen123 <kmullen@csuchico.edu>
2024-11-03 21:14:07 -0800

    Update main.dart

--- autocareconnect/lib/main.dart
+++ autocareconnect/lib/main.dart
@@ -7,6 +7,8 @@ import 'router.gr.dart';
 import 'pages/home_page.dart'; // HomePage
 import 'pages/login_page.dart'; // LoginPage
 import 'pages/signup_page.dart'; // SignUpPage
+import 'package:camera/camera.dart';
+import 'package:location/location.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +24,11 @@ class MyApp extends StatelessWidget {
   // Create the AppRouter instance
   final _appRouter = AppRouter();
 
+class _MyHomePageState extends State<MyHomePage> {
+    CameraController? _cameraController;
+    LocationData? _locationData;
+    List<String> _images = [];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
@@ -32,6 +39,105 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+  @override
+    void initState() {
+      super.initState();
+      _initializeCamera();
+      _initializeLocation();
+    }
+
+   Future<void> _initializeCamera() async {
+      final cameras = await availableCameras();
+      _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+      await _cameraController!.initialize();
+    }
+
+
+    Future<void> _initializeLocation() async {
+      final location = Location();
+      bool serviceEnabled;
+      PermissionStatus permissionStatus;
+
+
+      serviceEnabled = await location.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await location.requestService();
+        if (!serviceEnabled) {
+          return;
+        }
+      }
+
+
+      permissionStatus = await location.hasPermission();
+      if (permissionStatus == PermissionStatus.denied) {
+        permissionStatus = await location.requestPermission();
+        if (permissionStatus != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+
+      location.onLocationChanged.listen((LocationData newLocation) {
+        setState(() {
+          _locationData = newLocation;
+        });
+      });
+    }
+
+
+    Future<void> _takePhoto() async {
+      if (!_cameraController!.value.isInitialized) {
+        return;
+      }
+
+
+      try {
+        final XFile image = await _cameraController!.takePicture();
+        setState(() {
+          _images.add(image.path);
+        });
+      } catch (e) {
+        print('Error taking a photo: $e');
+      }
+    }
+
+   @override
+    void dispose() {
+      _cameraController?.dispose();
+      super.dispose();
+    }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Select Location for Service'),
+          ),
+        body: Column(
+          children: [
+            _locationData != null
+                ? Text('Location: ${_locationData!.latitude}, ${_locationData!.longitude}')
+                : Text('Location data not available'),
+
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: _images.length,
+                itemBuilder: (context, index) {
+                  return Image.file(File(_images[index]));
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _takePhoto,
+          child: Icon(Icons.camera),
+        ),
+
+
     );
   }
+  
 }
