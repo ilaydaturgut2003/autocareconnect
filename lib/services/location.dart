a7ff635fa50767b573c7c44a4710f188f77e35ca
KyleMullen123 <kmullen@csuchico.edu>
2024-12-16 11:55:27 -0800

    Update location.dart

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,97 +1,135 @@
+import 'dart:io';
 import 'package:location/location.dart';
-
-class LocationManager {
-  Location location = Location();
-
-  Future<bool> checkPermissions() async {
-    bool serviceEnabled = await location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return false;
-      }
-    }
-
-    PermissionStatus permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  Future<LocationData?> getCurrentLocation() async {
-    return await location.getLocation();
-  }
-}
-
-
-/*import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:autocareconnect/services/camera.dart';
-import 'package:autocareconnect/services/location.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:camera/camera.dart';
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
+    runApp(MyApp());
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
 
-class _MyHomePageState extends State<MyHomePage> {
-  LocationData? _locationData;
-  List<String> _images = [];
-
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeLocation();
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: MyHomePage(),
+      );
+    }
   }
-}
 
-class LocationManager {
-  Location location = new Location();
+
+  class MyHomePage extends StatefulWidget {
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+  }
+
+
+  class _MyHomePageState extends State<MyHomePage> {
+    CameraController? _cameraController;
+    LocationData? _locationData;
+    List<String> _images = [];
+
+
+    @override
+    void initState() {
+      super.initState();
+      _initializeCamera();
+      _initializeLocation();
+    }
+
+
+    Future<void> _initializeCamera() async {
+      final cameras = await availableCameras();
+      _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+      await _cameraController!.initialize();
+    }
+
+
+    Future<void> _initializeLocation() async {
+      final location = Location();
+      bool serviceEnabled;
+      PermissionStatus permissionStatus;
+
+
+      serviceEnabled = await location.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await location.requestService();
+        if (!serviceEnabled) {
+          return;
+        }
+      }
+
+
+      permissionStatus = await location.hasPermission();
+      if (permissionStatus == PermissionStatus.denied) {
+        permissionStatus = await location.requestPermission();
+        if (permissionStatus != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+
+      location.onLocationChanged.listen((LocationData newLocation) {
+        setState(() {
+          _locationData = newLocation;
+        });
+      });
+    }
+
+
+    Future<void> _takePhoto() async {
+      if (!_cameraController!.value.isInitialized) {
+        return;
+      }
+
+
+      try {
+        final XFile image = await _cameraController!.takePicture();
+        setState(() {
+          _images.add(image.path);
+        });
+      } catch (e) {
+        print('Error taking a photo: $e');
+      }
+    }
+
+
+    @override
+    void dispose() {
+      _cameraController?.dispose();
+      super.dispose();
+    }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('AutoCare Connect: Use camera and location to locate your car'),
+        ),
+        body: Column(
+          children: [
+            _locationData != null
+                ? Text('Location: ${_locationData!.latitude}, ${_locationData!.longitude}')
+                : Text('Location data not available'),
+
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: _images.length,
+                itemBuilder: (context, index) {
+                  return Image.file(File(_images[index]));
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _takePhoto,
+          child: Icon(Icons.camera),
+        ),
+      );
+    }
+  }
   
-  bool? _serviceEnabled;
-  PermissionStatus? _permissionGranted;
-  LocationData? _locationData;
 
-  Future<void> checkPermissions() async {
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled!) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled!) {
-        return;
-      }
-    }
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-  }
-
-  Future<LocationData?> getLocation() async {
-    _locationData = await location.getLocation();
-    return _locationData;
-  }
-}
-*/
